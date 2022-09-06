4a0a41bdd9ed838e3dbcc68ad3c0529c05ffef74
Abdulla Abdurakhmanov <me@abdolence.dev>
2022-09-06 22:41:54 +0200

    Implementation of serde::Serializer for FirestoreValueSerializer

--- src/serde_native_serializer.rs
+++ src/serde_native_serializer.rs
@@ -1,4 +1,5 @@
 use crate::FirestoreError;
+use gcloud_sdk::google::firestore::v1::value;
 use serde::Serialize;
 use std::collections::HashMap;
 
@@ -37,159 +38,204 @@ impl serde::Serializer for FirestoreValueSerializer {
     type SerializeStructVariant = SerializeStructVariant;
 
     fn serialize_bool(self, v: bool) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Self::Ok {
+            value_type: Some(value::ValueType::BooleanValue(v)),
+        })
     }
 
     fn serialize_i8(self, v: i8) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Self::Ok {
+            value_type: Some(value::ValueType::IntegerValue(v.into())),
+        })
     }
 
     fn serialize_i16(self, v: i16) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Self::Ok {
+            value_type: Some(value::ValueType::IntegerValue(v.into())),
+        })
     }
 
     fn serialize_i32(self, v: i32) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Self::Ok {
+            value_type: Some(value::ValueType::IntegerValue(v.into())),
+        })
     }
 
     fn serialize_i64(self, v: i64) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Self::Ok {
+            value_type: Some(value::ValueType::IntegerValue(v.into())),
+        })
     }
 
     fn serialize_u8(self, v: u8) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Self::Ok {
+            value_type: Some(value::ValueType::IntegerValue(v.into())),
+        })
     }
 
     fn serialize_u16(self, v: u16) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Self::Ok {
+            value_type: Some(value::ValueType::IntegerValue(v.into())),
+        })
     }
 
     fn serialize_u32(self, v: u32) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Self::Ok {
+            value_type: Some(value::ValueType::IntegerValue(v.into())),
+        })
     }
 
     fn serialize_u64(self, v: u64) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Self::Ok {
+            value_type: Some(value::ValueType::IntegerValue(v as i64)),
+        })
     }
 
     fn serialize_f32(self, v: f32) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Self::Ok {
+            value_type: Some(value::ValueType::DoubleValue(v.into())),
+        })
     }
 
     fn serialize_f64(self, v: f64) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Self::Ok {
+            value_type: Some(value::ValueType::DoubleValue(v.into())),
+        })
     }
 
     fn serialize_char(self, v: char) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Self::Ok {
+            value_type: Some(value::ValueType::StringValue(v.to_string())),
+        })
     }
 
     fn serialize_str(self, v: &str) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Self::Ok {
+            value_type: Some(value::ValueType::StringValue(v.to_string())),
+        })
     }
 
     fn serialize_bytes(self, v: &[u8]) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Self::Ok {
+            value_type: Some(value::ValueType::BytesValue(v.into())),
+        })
     }
 
     fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Self::Ok { value_type: None })
     }
 
     fn serialize_some<T: ?Sized>(self, value: &T) -> Result<Self::Ok, Self::Error>
     where
         T: Serialize,
     {
-        todo!()
+        value.serialize(self)
     }
 
     fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Self::Ok { value_type: None })
     }
 
-    fn serialize_unit_struct(self, name: &'static str) -> Result<Self::Ok, Self::Error> {
-        todo!()
+    fn serialize_unit_struct(self, _name: &'static str) -> Result<Self::Ok, Self::Error> {
+        self.serialize_unit()
     }
 
     fn serialize_unit_variant(
         self,
-        name: &'static str,
-        variant_index: u32,
+        _name: &'static str,
+        _variant_index: u32,
         variant: &'static str,
     ) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        self.serialize_str(variant)
     }
 
     fn serialize_newtype_struct<T: ?Sized>(
         self,
-        name: &'static str,
+        _name: &'static str,
         value: &T,
     ) -> Result<Self::Ok, Self::Error>
     where
         T: Serialize,
     {
-        todo!()
+        value.serialize(self)
     }
 
     fn serialize_newtype_variant<T: ?Sized>(
         self,
-        name: &'static str,
-        variant_index: u32,
+        _name: &'static str,
+        _variant_index: u32,
         variant: &'static str,
         value: &T,
     ) -> Result<Self::Ok, Self::Error>
     where
         T: Serialize,
     {
-        todo!()
+        let mut fields = HashMap::new();
+        fields.insert(String::from(variant), value.serialize(self)?);
+        Ok(Self::Ok {
+            value_type: Some(value::ValueType::MapValue(
+                gcloud_sdk::google::firestore::v1::MapValue { fields },
+            )),
+        })
     }
 
     fn serialize_seq(self, len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
-        todo!()
+        Ok(SerializeVec {
+            vec: Vec::with_capacity(len.unwrap_or(0)),
+        })
     }
 
     fn serialize_tuple(self, len: usize) -> Result<Self::SerializeTuple, Self::Error> {
-        todo!()
+        self.serialize_seq(Some(len))
     }
 
     fn serialize_tuple_struct(
         self,
-        name: &'static str,
+        _name: &'static str,
         len: usize,
     ) -> Result<Self::SerializeTupleStruct, Self::Error> {
-        todo!()
+        self.serialize_seq(Some(len))
     }
 
     fn serialize_tuple_variant(
         self,
-        name: &'static str,
-        variant_index: u32,
+        _name: &'static str,
+        _variant_index: u32,
         variant: &'static str,
         len: usize,
     ) -> Result<Self::SerializeTupleVariant, Self::Error> {
-        todo!()
+        Ok(SerializeTupleVariant {
+            name: String::from(variant),
+            vec: Vec::with_capacity(len),
+        })
     }
 
     fn serialize_map(self, len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
-        todo!()
+        Ok(SerializeMap::Map {
+            map: HashMap::with_capacity(len.unwrap_or(0)),
+            next_key: None,
+        })
     }
 
     fn serialize_struct(
         self,
-        name: &'static str,
+        _name: &'static str,
         len: usize,
     ) -> Result<Self::SerializeStruct, Self::Error> {
-        todo!()
+        self.serialize_map(Some(len))
     }
 
     fn serialize_struct_variant(
         self,
-        name: &'static str,
-        variant_index: u32,
+        _name: &'static str,
+        _variant_index: u32,
         variant: &'static str,
         len: usize,
     ) -> Result<Self::SerializeStructVariant, Self::Error> {
-        todo!()
+        Ok(SerializeStructVariant {
+            name: String::from(variant),
+            map: HashMap::with_capacity(len),
+        })
     }
 }
 
