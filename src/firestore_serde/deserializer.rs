f5f1a8b88d8f2c525edad441071c15615030dc3c
Abdulla Abdurakhmanov <me@abdolence.dev>
2022-10-24 17:47:30 +0200

    (cargo-release) start next development iteration 0.12.7-alpha.0

--- src/firestore_serde/deserializer.rs
+++ src/firestore_serde/deserializer.rs
@@ -495,8 +495,8 @@ impl<'de> serde::Deserializer<'de> for FirestoreValue {
     where
         V: Visitor<'de>,
     {
-        visitor.visit_unit()
-        //self.deserialize_any(visitor)
+        //visitor.visit_unit()
+        self.deserialize_any(visitor)
     }
 
     fn deserialize_unit_struct<V>(
