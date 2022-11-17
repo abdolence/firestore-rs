42f456b9027678266826f2ee0d8e57f12f856c6f
Nick Caplinger <the.nick.caplinger@gmail.com>
2022-11-16 21:25:51 -0600

    Introduce run_transaction_with_options

--- src/db/transaction.rs
+++ src/db/transaction.rs
@@ -3,8 +3,8 @@ use std::pin::Pin;
 use crate::errors::FirestoreDatabaseError;
 use crate::timestamp_utils::from_timestamp;
 use crate::{FirestoreConsistencySelector, FirestoreDb, FirestoreError, FirestoreResult};
-use backoff::ExponentialBackoff;
 use backoff::future::retry;
+use backoff::ExponentialBackoff;
 use gcloud_sdk::google::firestore::v1::{BeginTransactionRequest, CommitRequest, RollbackRequest};
 use rsb_derive::Builder;
 use tracing::*;
@@ -13,6 +13,14 @@ use tracing::*;
 pub struct FirestoreTransactionOptions {
     #[default = "FirestoreTransactionMode::ReadWrite"]
     pub mode: FirestoreTransactionMode,
+}
+
+impl Default for FirestoreTransactionOptions {
+    fn default() -> Self {
+        Self {
+            mode: FirestoreTransactionMode::ReadWrite,
+        }
+    }
 }
 
 impl TryFrom<FirestoreTransactionOptions>
@@ -216,10 +224,26 @@ impl FirestoreDb {
         )
             -> Pin<Box<dyn futures::Future<Output = Result<T, FirestoreError>> + 'a>>,
     {
+        let options = FirestoreTransactionOptions::default();
+        self.run_transaction_with_options(func, options).await
+    }
+
+    pub async fn run_transaction_with_options<T, F>(
+        &self,
+        func: F,
+        options: FirestoreTransactionOptions,
+    ) -> Result<T, FirestoreError>
+    where
+        F: for<'a> Fn(
+            FirestoreDb,
+            &'a mut FirestoreTransaction,
+        )
+            -> Pin<Box<dyn futures::Future<Output = Result<T, FirestoreError>> + 'a>>,
+    {
         // Perform our initial attempt. If this fails and the backend tells us we can retry,
         // we'll try again with exponential backoff using the first attempt's transaction ID.
         let transaction_id = {
-            let mut transaction = self.begin_transaction().await?;
+            let mut transaction = self.begin_transaction_with_options(options).await?;
 
             let cdb = self.clone_with_consistency_selector(
                 FirestoreConsistencySelector::Transaction(transaction.transaction_id.clone()),
@@ -236,7 +260,7 @@ impl FirestoreDb {
                         retry_possible: true,
                         ..
                     }) => {
-                        // Ignore; we'll try again
+                        // Ignore; we'll try again below
                     }
                     FirestoreError::DatabaseError(FirestoreDatabaseError {
                         retry_possible: false,
@@ -252,7 +276,7 @@ impl FirestoreDb {
         };
 
         // We failed the first time. Now we must change the transaction mode to signal that we're retrying with the original transaction ID.
-        let result = retry(ExponentialBackoff::default(), || async {
+        retry(ExponentialBackoff::default(), || async {
             let options = FirestoreTransactionOptions {
                 mode: FirestoreTransactionMode::ReadWriteRetry(transaction_id.clone()),
             };
@@ -265,30 +289,25 @@ impl FirestoreDb {
             let ret_val = func(cdb, &mut transaction).await?;
 
             match transaction.commit().await {
-                Ok(_) => return Ok(ret_val),
+                Ok(_) => Ok(ret_val),
                 Err(e) => match e {
                     FirestoreError::DatabaseError(FirestoreDatabaseError {
                         retry_possible: true,
                         ..
                     }) => {
                         eprintln!("Got back retryable error: {e}");
-                        return Err(backoff::Error::transient(e));
+                        Err(backoff::Error::transient(e))
                     }
                     FirestoreError::DatabaseError(FirestoreDatabaseError {
                         retry_possible: false,
                         ..
-                    }) => {
-                        return Err(backoff::Error::permanent(e));
-                    }
-                    e => return Err(backoff::Error::permanent(e)),
+                    }) => Err(backoff::Error::permanent(e)),
+                    e => Err(backoff::Error::permanent(e)),
                 },
             }
         })
-        .await;
+        .await
 
         // TODO If the final result was Err, do we still need to call `rollback`?
-
-        result
     }
-
 }
