c4b9f307aa39095cc8ffd9b2197485ad27b4dcf9
Abdulla Abdurakhmanov <me@abdolence.dev>
2023-08-10 18:44:32 +0200

    Multiple databases support

--- src/db/options.rs
+++ src/db/options.rs
@@ -4,8 +4,13 @@ use rsb_derive::Builder;
 pub struct FirestoreDbOptions {
     pub google_project_id: String,
 
+    #[default = "FIREBASE_DEFAULT_DATABASE_ID.to_string()"]
+    pub database_id: String,
+
     #[default = "3"]
     pub max_retries: usize,
 
     pub firebase_api_url: Option<String>,
 }
+
+pub const FIREBASE_DEFAULT_DATABASE_ID: &str = "(default)";
