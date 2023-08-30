ce756a2f6ad890d4520d30a05ad50c6b9bed8521
Abdulla Abdurakhmanov <me@abdolence.dev>
2023-08-30 21:48:11 +0200

    Fixed deprecated warning for chrono

--- src/timestamp_utils.rs
+++ src/timestamp_utils.rs
@@ -4,7 +4,7 @@ use chrono::prelude::*;
 
 pub fn from_timestamp(ts: prost_types::Timestamp) -> FirestoreResult<DateTime<Utc>> {
     if let Some(dt) = chrono::NaiveDateTime::from_timestamp_opt(ts.seconds, ts.nanos as u32) {
-        Ok(DateTime::<Utc>::from_utc(dt, Utc))
+        Ok(DateTime::<Utc>::from_naive_utc_and_offset(dt, Utc))
     } else {
         Err(FirestoreError::DeserializeError(
             FirestoreSerializationError::from_message(format!(
