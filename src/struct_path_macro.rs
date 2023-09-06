688564d9d405b2d7a6f10b58b46090e9488c9478
bouzuya <bouzuya@users.noreply.github.com>
2023-09-07 06:41:08 +0900

    Fix to use $crate in path macros (#124)

--- src/struct_path_macro.rs
+++ src/struct_path_macro.rs
@@ -1,27 +1,27 @@
 #[macro_export]
 macro_rules! path {
     ($($x:tt)*) => {{
-        struct_path::path!($($x)*).to_string()
+        $crate::struct_path::path!($($x)*).to_string()
     }};
 }
 
 #[macro_export]
 macro_rules! paths {
     ($($x:tt)*) => {{
-        struct_path::paths!($($x)*).iter().map(|s| s.to_string()).collect::<Vec<String>>()
+        $crate::struct_path::paths!($($x)*).iter().map(|s| s.to_string()).collect::<Vec<String>>()
     }};
 }
 
 #[macro_export]
 macro_rules! path_camel_case {
     ($($x:tt)*) => {{
-        struct_path::path!($($x)*;case="camel").to_string()
+        $crate::struct_path::path!($($x)*;case="camel").to_string()
     }};
 }
 
 #[macro_export]
 macro_rules! paths_camel_case {
     ($($x:tt)*) => {{
-        struct_path::paths!($($x)*;case="camel").into_iter().map(|s| s.to_string()).collect::<Vec<String>>()
+        $crate::struct_path::paths!($($x)*;case="camel").into_iter().map(|s| s.to_string()).collect::<Vec<String>>()
     }}
 }
