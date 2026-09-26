8f08dfe4e89d3f0fa40c56955cb976c67ec5f5ce
abdolence <abdolence@users.noreply.github.com>
2026-09-26 15:20:00 +0000

    deploy: a629994acaeb806d5b641d2ab6b13fec97ff8063

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4e8cffbb.js',
+                path_to_root + 'searchindex-6d9d7ae7.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
